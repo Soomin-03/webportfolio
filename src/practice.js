3553ce03e83ea8610b12b21054408eaa7c10eecd
Park SooMin <pssc4244@gmail.com>
2025-07-10 14:29:26 +0900

    feat:practice5

---
+++ src/practice.js
@@ -0,0 +1,25 @@
+export default function printMyself() {
+    function showMyself() {
+        <button
+            onClick={() => {
+                console.log("Name : Park SooMin");
+                console.log("Age : 23");
+            }}
+            style={{
+                color: 'white',
+                backgroundColor: 'Black'
+            }}
+        >
+            Myself
+        </button>
+    }
+    function showMyportfolio() {
+        <button
+            onClick={() => {
+                console.log("Sorry");
+            }}
+        >
+            protfolio
+        </button>
+    }
+}
