e713831240213997826e06a8fe2121b238b30efd
Park SooMin <pssc4244@gmail.com>
2025-07-10 14:40:27 +0900

    feat:practice5rere

--- src/practice.js
+++ src/practice.js
@@ -22,4 +22,10 @@ export default function printMyself() {
             protfolio
         </button>
     }
+    return (
+        <div>
+            <showMyself/>
+            <showMyportfolio/>
+        </div>
+    )
 }
